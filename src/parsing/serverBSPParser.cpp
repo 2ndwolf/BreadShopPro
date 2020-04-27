f4e64fbb49c3a33b09d5942a01fde95bc8b9bc81
Dominic Grenier <themoustafa@hotmail.com>
2020-04-27 16:58:13 -0400

    Boilerplate

---
+++ src/parsing/serverBSPParser.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "src/components/ganiInformation.h"
+#include "include/stringUtils.h"
+#include "include/enums.h"
+#include "include/ganiParser.h"
+
+using namespace std;
+using namespace GaniBuilding;
+
+namespace Parsing {
+  cGaniInformation parseServerBSP(vector<string> lines) {
+
+  }
+}
