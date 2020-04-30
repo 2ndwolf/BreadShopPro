bda8fc36030b6ba5a1fc981fd0532207acf08a5a
Dominic Grenier <themoustafa@hotmail.com>
2020-04-30 15:45:16 -0400

    SDL2 integration

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include <google/protobuf/io/zero_copy_stream_impl_lite.h>
 #include <google/protobuf/io/gzip_stream.h>
+#define SDL_MAIN_HANDLED
+#include <dependencies/SDL2/SDL.h> 
+#include <dependencies/SDL2/SDL_image.h> 
+#include <dependencies/SDL2/SDL_timer.h>
 
 #include "components/ganiInformation.h"
 #include "include/ganiParser.h"
@@ -19,14 +23,136 @@ using namespace GaniBuilding;
 
 int main() {
 
-  // std::string protobufPath("bob.bsp");
-  // std::string bitmapPath("Mona_Lisa_decompressed.bmp");
-  // std::ifstream in(protobufPath, std::ios::binary);
+    // returns zero on success else non-zero 
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) { 
+        printf("error initializing SDL: %s\n", SDL_GetError()); 
+    } 
+    SDL_Window* win = SDL_CreateWindow("GAME", 
+                                       SDL_WINDOWPOS_CENTERED, 
+                                       SDL_WINDOWPOS_CENTERED, 
+                                       1000, 1000, 0); 
+    // triggers the program that controls 
+    // your graphics hardware and sets flags 
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED; 
+  
+    // creates a renderer to render our images 
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags); 
+  
+    // creates a surface to load an image into the main memory 
+    SDL_Surface* surface; 
+  
+    // please provide a path for your image 
+    surface = IMG_Load("assets/gen_specialchest.gif"); 
+  
+    // loads image to our graphics hardware memory. 
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface); 
+  
+    // clears main-memory 
+    SDL_FreeSurface(surface); 
+  
+    // let us control our image position 
+    // so that we can move it with our keyboard. 
+    SDL_Rect dest; 
+  
+    // connects our texture with dest to control position 
+    SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h); 
+  
+    // adjust height and width of our image box. 
+    // dest.w /= 6; 
+    // dest.h /= 6; 
+  
+    // sets initial x-position of object 
+    dest.x = (1000 - dest.w) / 2; 
+  
+    // sets initial y-position of object 
+    dest.y = (1000 - dest.h) / 2; 
+  
+    // controls annimation loop 
+    int close = 0; 
+  
+    // speed of box 
+    int speed = 300; 
+  
+    // annimation loop 
+    while (!close) { 
+        SDL_Event event; 
+  
+        // Events mangement 
+        while (SDL_PollEvent(&event)) { 
+            switch (event.type) { 
+  
+            case SDL_QUIT: 
+                // handling of close button 
+                close = 1; 
+                break; 
+  
+            case SDL_KEYDOWN: 
+                // keyboard API for key pressed 
+                switch (event.key.keysym.scancode) { 
+                case SDL_SCANCODE_W: 
+                case SDL_SCANCODE_UP: 
+                    dest.y -= speed / 30; 
+                    break; 
+                case SDL_SCANCODE_A: 
+                case SDL_SCANCODE_LEFT: 
+                    dest.x -= speed / 30; 
+                    break; 
+                case SDL_SCANCODE_S: 
+                case SDL_SCANCODE_DOWN: 
+                    dest.y += speed / 30; 
+                    break; 
+                case SDL_SCANCODE_D: 
+                case SDL_SCANCODE_RIGHT: 
+                    dest.x += speed / 30; 
+                    break; 
+                } 
+            } 
+        } 
+  
+        // right boundary 
+        if (dest.x + dest.w > 1000) 
+            dest.x = 1000 - dest.w; 
+  
+        // left boundary 
+        if (dest.x < 0) 
+            dest.x = 0; 
+  
+        // bottom boundary 
+        if (dest.y + dest.h > 1000) 
+            dest.y = 1000 - dest.h; 
+  
+        // upper boundary 
+        if (dest.y < 0) 
+            dest.y = 0; 
+  
+        // clears the screen 
+        SDL_RenderClear(rend); 
+        SDL_RenderCopy(rend, tex, NULL, &dest); 
+  
+        // triggers the double buffers 
+        // for multiple rendering 
+        SDL_RenderPresent(rend); 
+  
+        // calculates to 60 fps 
+        SDL_Delay(1000 / 60); 
+    } 
+  
+    // destroy texture 
+    SDL_DestroyTexture(tex); 
+  
+    // destroy renderer 
+    SDL_DestroyRenderer(rend); 
+  
+    // destroy window 
+    SDL_DestroyWindow(win); 
+    return 0; 
+  }
 
 
-  GaniInformation currentAni = Opening::open("assets/yay.gani");
 
-  Saving::save(currentAni, "test", "serverbsp");
+  // GaniInformation currentAni = Opening::open("assets/test.serverbsp");
 
-  return 0;
-}
+  // Saving::save(currentAni, "test", "serverbsp");
+
+//   return 0;
+// }
